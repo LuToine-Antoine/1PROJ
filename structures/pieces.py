f123160a5f445c8c0fd9ca6276fb66f83c899fdc
Antoine <antoine.julien@supinfo.com>
2024-04-08 12:56:34 +0200

    commit board

--- structures/pieces.py
+++ structures/pieces.py
@@ -1,10 +1,4 @@
 from structures.board.board_struct import *
 from structures.board.board_ui import *
 
-for i in range(11):
-    for j in range(19):
-        if board[i][j] == 0:
-            print(" ", end='')
-        else:
-            print("o", end='')
-    print(" ")
+see_board()
