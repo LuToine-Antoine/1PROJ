5dc7f9bea7904bc25c977e41410ad04ff1221682
☾ Lu <72708201+Lu-LuToine@users.noreply.github.com>
2024-05-24 16:54:46 +0200

    debut jeu ui

--- ui/game_ui.py
+++ ui/game_ui.py
@@ -1,4 +1,6 @@
 import pygame as pygame
+from pygame.transform import scale
+
 
 class GameUI:
     def __init__(self, width=1280, height=720):
@@ -8,10 +10,23 @@ class GameUI:
 
         pygame.display.set_caption('Yinch')
         self._bg = pygame.image.load('../images/game/plateau_yinch.png').convert_alpha()
+        self._image = pygame.transform.scale(image, (int(self._width * scale), int(self._height * scale)))
 
     def get_screen(self):
         return self._screen
 
+    def window(self):
+        run = True
+        while run:
+
+            self.get_screen().blit(self._bg, (0, 0))
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    run = False
+            pygame.display.update()
+
 game_ui = GameUI()
 
 game_ui.get_screen()
+game_ui.window()
