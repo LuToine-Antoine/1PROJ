3cc807d968887bd5b83091f8d8c7368b5211ce30
Pouicc <poupouic2@gmail.com>
2024-05-24 16:14:04 +0200

    rings

--- possibilites/ringsmoves.py
+++ possibilites/ringsmoves.py
@@ -3,7 +3,6 @@ class RingsMoves:
         self._x = x
         self._y = y
         self._board = board
-        self._vertical_moves = []
         self._horizontal_moves = []
         self._right_diagonal_moves = []
         self._left_diagonal_moves = []
@@ -11,14 +10,24 @@ class RingsMoves:
     def set_horizontal_moves(self):
         self._horizontal_moves.clear()
         for j in range(len(self._board[self._x])):
-            if self._board[self._x][j] == 4 or self._board[self._x][j] == 5:
-                self._horizontal_moves.append((j+1, self._x+1))
-                break
-            elif self._board[self._x][j] == 2 or self._board[self._x][j] == 3:
-                break
-            elif self._board[self._x][j] == 1:
+            if self._board[self._x][j] == 1:
                 self._horizontal_moves.append((self._x, j))
-
+            if self._board[self._x][j] == 4 or self._board[self._x][j] == 5:
+                if self._y > j:
+                    for l in range(j,0,-1):
+                        self._horizontal_moves.pop((self._x, l))
+                elif self._y <= j:
+                    for l in range(j, len(self._board[self._x])):
+                        self._horizontal_moves.pop((self._x, l))
+        for k in range(len(self._board[self._x])):
+            if self._board[self._x][k] == 2 or self._board[self._x][k] == 3:
+                if self._y > k:
+                    for l in range(k):
+                        self._horizontal_moves.pop((self._x, l))
+                elif self._y <= k:
+                    for l in range(k, len(self._board[self._x])):
+                        self._horizontal_moves.pop((self._x, l))
+                
     def get_horizontal_moves(self):
         return self._horizontal_moves
 
@@ -27,13 +36,12 @@ class RingsMoves:
         min_left = min(self._x + 1, self._y + 1)  # For no out of range
         for i in range(1, min_left):
             if self._x - i < len(self._board) and self._y - i < len(self._board[0]):
-                if self._board[self._x - i][self._y - i] == 4 or self._board[self._x - i][self._y - i] == 5:
+                if self._board[self._x - i][self._y - i] == 1:
                     self._right_diagonal_moves.append((self._x - i, self._y - i))
+        for i in range(1, min_left):
+            if self._x - i < len(self._board) and self._y - i < len(self._board[0]):
+                if self._board[self._x - i][self._y - i] == 2 or self._board[self._x - i][self._y - i] == 3:
                     break
-                elif self._board[self._x - i][self._y - i] == 2 or self._board[self._x - i][self._y - i] == 3:
-                    break
-                elif self._board[self._x - i][self._y - i] == 1:
-                    self._right_diagonal_moves.append((self._x - i, self._y - i))
 
         max_right = min(len(self._board) - self._x, len(self._board[0]) - self._y)  # For no out of range
 
@@ -58,6 +66,7 @@ class RingsMoves:
                 self._left_diagonal_moves.append((self._x - i, self._y + i))
 
         max_left = min(len(self._board) - self._x, self._y + 1)  # For no out of range, bottom-left to top-right
+
         for i in range(1, max_left):
             if self._x + i < len(self._board) and self._y - i >= 0 and self._board[self._x + i][self._y - i] == 1:
                 self._left_diagonal_moves.append((self._x + i, self._y - i))
@@ -68,8 +77,7 @@ class RingsMoves:
     def get_possible_moves(self, x, y):
         self._x = x
         self._y = y
-        self.set_vertical_moves()
         self.set_horizontal_moves()
         self.set_right_diagonal_moves()
         self.set_left_diagonal_moves()
-        return self.get_vertical_moves(), self.get_horizontal_moves(), self.get_right_diagonal_moves(), self.get_left_diagonal_moves()
+        return self.get_horizontal_moves(), self.get_right_diagonal_moves(), self.get_left_diagonal_moves()
