3a6d0655290aa330718dbd794e8face3e157af52
☾ Lu <72708201+Lu-LuToine@users.noreply.github.com>
2024-05-28 14:35:44 +0200

    ajout des pions sur le plateau en fonction du chiffre et de l'image

--- ui/game_ui.py
+++ ui/game_ui.py
@@ -24,9 +24,9 @@ class GameUI:
         while run:
             self._screen.fill((255, 255, 255))
 
-            self.afficher_plateau()
 
             self.get_screen().blit(self.board(), (0, 0))
+            self.afficher_plateau()
 
             # get mouse position
             pos = pygame.mouse.get_pos()
@@ -40,19 +40,42 @@ class GameUI:
             pygame.display.update()
 
     def afficher_plateau(self):
-        #ring_1 = pygame.image.load('../images/game/pawn_and_ring_1.png').convert_alpha()
-        sizex, sizey = 100, 100
-        #look_1 = pygame.transform.scale(ring_1, (sizex, sizey))
+        pawn_ring_1 = pygame.image.load('images/game/pawn_and_ring_1.png').convert_alpha()
+        pawn_ring_2 = pygame.image.load('images/game/pawn_and_ring_2.png').convert_alpha()
+        pawn_1 = pygame.image.load('images/game/pawn_player_1.png').convert_alpha()
+        pawn_2 = pygame.image.load('images/game/pawn_player_2.png').convert_alpha()
+        ring_1 = pygame.image.load('images/game/ring_player_1.png').convert_alpha()
+        ring_2 = pygame.image.load('images/game/ring_player_2.png').convert_alpha()
+        sizex, sizey = 80, 80
+        pawn_ring_1 = pygame.transform.scale(pawn_ring_1, (sizex, sizey))
+        pawn_ring_2 = pygame.transform.scale(pawn_ring_2, (sizex, sizey))
+        ring_1 = pygame.transform.scale(ring_1, (sizex, sizey))
+        ring_2 = pygame.transform.scale(ring_2, (sizex, sizey))
+
+        sizex, sizey = 90, 90
+        pawn_1 = pygame.transform.scale(pawn_1, (sizex, sizey))
+        pawn_2 = pygame.transform.scale(pawn_2, (sizex, sizey))
+
+        #for i in range(len(self._main.get_board()[0])):
+        #    for j in range(len(self._main.get_board())):
+        #        pygame.draw.rect(self._screen, (255, 0, 0), (27 + j * 54, 15 + i * 33, 54, 33))
         for i in range(len(self._main.get_board()[0])):
             for j in range(len(self._main.get_board())):
-                if self._main.get_board()[j][i] == 1:
-                    pygame.draw.rect(self._screen, (255, 255, 0), (27 + j * 54, 15 + i * 33, 54, 33), 1)
-                    pass
-                else:
-                    pygame.draw.rect(self._screen, (255, 0, 0), (27 + j * 54, 15 + i * 33, 54, 33))
+                if self._main.get_board()[j][i] == 2:
+                    self._screen.blit(ring_1, (10 + j * 54, i * 33))
+                elif self._main.get_board()[j][i] == 3:
+                    self._screen.blit(ring_2, (10 + j * 54, i * 33))
+                elif self._main.get_board()[j][i] == 4:
+                    self._screen.blit(pawn_1, (10 + j * 54, i * 33))
+                elif self._main.get_board()[j][i] == 5:
+                    self._screen.blit(pawn_2, (j * 54, (-5) + i * 33))
+                elif self._main.get_board()[j][i] == 6:
+                    self._screen.blit(pawn_ring_1, (10 + j * 54, i * 33))
+                elif self._main.get_board()[j][i] == 7:
+                    self._screen.blit(pawn_ring_2, (10 + j * 54, i * 33))
 
 
 
-# game_ui = GameUI()
-# game_ui.get_screen()
-# game_ui.window()
+game_ui = GameUI()
+#game_ui.get_screen()
+#game_ui.window()
