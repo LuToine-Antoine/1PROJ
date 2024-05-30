12d8d8b183013ef5c4f94537a9ef873a8f691c0b
Pouicc <poupouic2@gmail.com>
2024-05-30 23:02:35 +0200

    horizontal

--- possibilites/ringsmoves.py
+++ possibilites/ringsmoves.py
@@ -20,8 +20,9 @@ class RingsMoves:
                     for i2 in range(self._x + 1, i):
                         if self._board[i2][self._y] == 1:
                             self._vertical_moves.append((i2, self._y))
-                    if i + 2 < len(self._board) and self._board[i + 2][self._y] != 1:
+                    if i + 2 < len(self._board) and self._board[i + 2][self._y] == 1:
                         self._vertical_moves.append((i + 2, self._y))
+                        break
                 elif self._board[i][self._y] in [2, 3]:
                     for i3 in range(self._x + 1, i):
                         if self._board[i3][self._y] == 1:
@@ -39,8 +40,9 @@ class RingsMoves:
                     for j2 in range(j + 1, self._x):
                         if self._board[j2][self._y] == 1:
                             self._vertical_moves.append((j2, self._y))
-                    if j - 2 >= 0 and self._board[j - 2][self._y] != 1:
+                    if j - 2 >= 0 and self._board[j - 2][self._y] == 1:
                         self._vertical_moves.append((j - 2, self._y))
+                        break
                 elif self._board[j][self._y] in [2, 3]:
                     for j3 in range(j + 1, self._x):
                         if self._board[j3][self._y] == 1:
