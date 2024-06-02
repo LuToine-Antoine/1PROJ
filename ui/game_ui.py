36010e9c685aaacc7cb20e6609088384b7db2ef9
Antoine <antoine.julien@supinfo.com>
2024-06-02 12:25:03 +0200

    text tips

--- ui/game_ui.py
+++ ui/game_ui.py
@@ -53,11 +53,13 @@ class GameUI:
         selected_player = pygame.transform.scale(
             selected_player_img, (int(self._screen_width * 0.04), int(self._screen_height * 0.06)))
 
-        text_tips = font_title.render('Astuce : Cliquez droit sur votre anneau pour voir vos déplacements possibles.', True, blue)
+        text_tips = font_action.render('Astuce : Cliquez droit sur votre anneau pour voir vos déplacements possibles.', True, black)
 
         text_player_1 = font_title.render('Joueur 1', True, blue)
-        # text_ring_number_1 = font_title.render(f'Pawn number {self._main.get_player_1_ring()}', True, red)
         text_player_2 = font_title.render('Joueur 2', True, red)
+
+        text_ring_number_1 = font_title.render(f'Pawn number {self._main.get_player_1_ring()}', True, red)
+
 
         run = True
         while run:
@@ -129,11 +131,7 @@ class GameUI:
             self.get_screen().blit(text_player_1, (840, 150))
             self.get_screen().blit(text_player_2, (840, 300))
 
-            self.get_screen().blit(text_tips, (840, 300))
-
-
-            # if self._main.get_player() < 10:
-            #     self.get_screen().blit(text_tips, (860, 30))
+            self.get_screen().blit(text_tips, (450, 650))
 
             if self._main.get_player() == 1:
                 self.get_screen().blit(selected_player, (780, 145))
