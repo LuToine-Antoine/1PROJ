06f4f8f914fe07efc150d543f6249dc026cee84d
☾ Lu <72708201+Lu-LuToine@users.noreply.github.com>
2024-06-02 04:04:24 +0200

    win possible

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ class Game:
         self._rotation = None
         self._mode = None
         self._game_mode = None
-        self._change_player = True
         self._player_1_out_ring = 0
         self._player_2_out_ring = 0
         self._player_1_align = 0
@@ -119,9 +118,6 @@ class Game:
         self._click_y = y
 
         if self.in_board_verification(self._click_x, self._click_y) :
-            if self._round == 10:
-                self._player = 1
-
             if self._player == 1:
                 caseplayer = 2
             else:
@@ -137,31 +133,30 @@ class Game:
                             self.main_see_moves_rings()
                             self._clickCount = 1
 
+            if self._clickCount == 1:
+                if self.main_move_rings(x, y, self._player):
+                    self._board.see_board()
+                    self._round += 1
+                    if self._player == 1:
+                        self._player = 2
+                    else:
+                        self._player = 1
+                    self._clickCount = 0
         # Check if a player can remove a ring and add ring in his ring out stock
+            align_result = self.alignement()
+            if align_result == 1 and self._player_1_align > self._player_1_out_ring:
+                self.choix_anneaux(1)
+                if (self._click_x, self._click_y) in self._choix:
+                    self.ring_out(self._click_x, self._click_y, 1)
+
+            elif align_result == 2 and self._player_2_align > self._player_2_out_ring:
+                self.choix_anneaux(2)
+                if (self._click_x, self._click_y) in self._choix:
+                        self.ring_out(self._click_x, self._click_y, 2)
 
 
-        align_result = self.alignement()
-        if align_result == 1 and self._player_1_align > self._player_1_out_ring:
-            self._change_player = False
-            self.choix_anneaux(1)
-            if (self._click_x, self._click_y) in self._choix:
-                self.ring_out(self._click_x, self._click_y, 1)
 
-        elif align_result == 2 and self._player_2_align > self._player_2_out_ring:
-            self._change_player = False
-            self.choix_anneaux(2)
-            if (self._click_x, self._click_y) in self._choix:
-                self.ring_out(self._click_x, self._click_y, 2)
 
-        if self._clickCount == 1 and self._change_player:
-            if self.main_move_rings(x, y, self._player):
-                self._board.see_board()
-                self._round += 1
-                if self._player == 1:
-                    self._player = 2
-                else:
-                    self._player = 1
-                self._clickCount = 0
 
 
             # Reset click
@@ -197,7 +192,6 @@ class Game:
             self._player_2_out_ring += 1
 
         self._board.board[x][y] = 1
-        self._change_player = True
 
 
     def in_board_verification(self, x, y):
